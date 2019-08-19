dd6031da29889c359a9893f0f959afc5bda89d23
CHOIMINSEOK <cms3718@gmail.com>
2019-08-20 00:53:40 +0900

    Link react-native-vector-icon & BottomTabIcon 추가

--- screen/AddContactScreen.js
+++ screen/AddContactScreen.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
 export default class AddContactScreen extends React.Component {
+  static navigationOptions = {
+    tabBarIcon: ({focused, tintColor}) => (
+      <Ionicons name={'ios-contacts'} size={25} color={tintColor} />
+    ),
+  };
   render() {
     return (
       <View style={styles.container}>
